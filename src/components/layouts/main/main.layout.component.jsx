3b4f1f56c7293cc19185af338a181ba973b6e657
Kazi Robin <kazirobinking@gmail.com>
2025-10-12 23:18:29 +0600

    hamburger

--- src/components/layouts/main/main.layout.component.jsx
+++ src/components/layouts/main/main.layout.component.jsx
@@ -2,14 +2,19 @@ import Sidebar from "../../shared/sidebar";
 import { Outlet } from "react-router";
 import NavbarComponent from "../../shared/navbar";
 import { Theme } from "@radix-ui/themes";
+import { useState } from "react";
+
 
 function MainLayout() {
+  const [sidebarActive,setSidebarActive]=useState(false)
+  const handleSidebar= () => { setSidebarActive(!sidebarActive) }
   return (
     <Theme>
       <div className="flex h-screen overflow-hidden">
-        <Sidebar />
+        {sidebarActive && <Sidebar handleSidebar={handleSidebar}/>}
+        
         <div className="flex-1">
-          <NavbarComponent />
+          <NavbarComponent handleSidebar={handleSidebar}/>
           <div className="h-screen overflow-scroll pb-40">
             <Outlet />
           </div>
--- src/components/shared/navbar/navbar.component.jsx
+++ src/components/shared/navbar/navbar.component.jsx
@@ -6,12 +6,16 @@ import {
   Text,
   TextField,
 } from "@radix-ui/themes";
+import { FaHamburger } from "react-icons/fa";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { IoIosNotifications } from "react-icons/io";
 
-const NavbarComponent = () => {
+const NavbarComponent = ({ handleSidebar }) => {
   return (
     <Flex justify="between" align="center" className="bg-amber-300 p-4" gap="4">
+      <Button onClick={handleSidebar}>
+        <FaHamburger />
+      </Button>
       <TextField.Root placeholder="Search..." size="2">
         <TextField.Slot>
           <FaMagnifyingGlass />
@@ -20,30 +24,30 @@ const NavbarComponent = () => {
 
       <Flex align="center" gap="4" className="cursor-pointer">
         <Button variant="ghost" size="2">
-          <IoIosNotifications className="text-blue-500 text-3xl"/>
+          <IoIosNotifications className="text-blue-500 text-3xl" />
         </Button>
 
-        <DropdownMenu.Root content="LogOut" side="bottom">
+        <DropdownMenu.Root>
           <DropdownMenu.Trigger>
-            <Flex gap="6px">
+            <Button variant="ghost">
+
+            <Flex gap="6px" align="center">
               <Avatar
                 src="https://avatars.githubusercontent.com/u/110531952?v=4"
-                alt="User avatar"
-                size="8"
+                fallback="K"
+                size="2"
                 radius="full"
-                fallback="U"
-                style={{
-                  width: "27px",
-                  height: "27px",
-                }}
-              />
+                />
               <Text>Kazi Robin</Text>
             </Flex>
+                </Button>
           </DropdownMenu.Trigger>
           <DropdownMenu.Content
             style={{ marginTop: "6px", marginInlineStart: "8px" }}
           >
-            <Button color="sky" style={{cursor:"pointer"}}>Log Out</Button>
+            <Button color="sky" style={{ cursor: "pointer" }}>
+              Log Out
+            </Button>
           </DropdownMenu.Content>
         </DropdownMenu.Root>
       </Flex>
